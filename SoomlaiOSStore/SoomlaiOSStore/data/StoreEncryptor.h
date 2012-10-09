557d048533706dfa2524b47260db3b3fb8014b6e
refaelos <refaeldakar@gmail.com>
2012-10-09 17:42:56 +0200

    finishing ios-store
    - Replaced encryption library to FBEncryptor
    (https://github.com/dev5tec/FBEncryptor)
    - Not using CoreData anymore (too slow). Using SQLite instead.
    - Added event handler for restore transactions
    - some bug fixes
    - purchasing was tested

--- SoomlaiOSStore/SoomlaiOSStore/data/StoreEncryptor.h
+++ SoomlaiOSStore/SoomlaiOSStore/data/StoreEncryptor.h
@@ -1,13 +1,13 @@
 //
-//  SQLiteDatabase.h
+//  StoreEncryptor.h
 //  SoomlaiOSStore
 //
-//  Created by Refael Dakar on 10/7/12.
+//  Created by Refael Dakar on 10/8/12.
 //  Copyright (c) 2012 SOOMLA. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface SQLiteDatabase : NSObject
+@interface StoreEncryptor : NSObject
 
 @end
---
+++ SoomlaiOSStore/SoomlaiOSStore/data/StoreEncryptor.m
@@ -0,0 +1,13 @@
+//
+//  StoreEncryptor.m
+//  SoomlaiOSStore
+//
+//  Created by Refael Dakar on 10/8/12.
+//  Copyright (c) 2012 SOOMLA. All rights reserved.
+//
+
+#import "StoreEncryptor.h"
+
+@implementation StoreEncryptor
+
+@end
