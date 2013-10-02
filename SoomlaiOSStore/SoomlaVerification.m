26164d39f4ebbdf621496629a75eafbc5a2f04a3
refaelos <refaeldakar@gmail.com>
2013-10-02 12:49:53 +0300

    checking for a valid certificate

--- SoomlaiOSStore/SoomlaVerification.m
+++ SoomlaiOSStore/SoomlaVerification.m
@@ -112,15 +112,15 @@ static NSString* TAG = @"SOOMLA SoomlaVerification";
     [EventHandling postUnexpectedError:ERR_VERIFICATION_TIMEOUT forObject:self];
 }
 
-- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
-    return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
-            [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
-    
-    [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
-}
+// - (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
+//     return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
+// }
+// 
+// - (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+//     if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
+//             [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+//     
+//     [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
+// }
 
 @end
