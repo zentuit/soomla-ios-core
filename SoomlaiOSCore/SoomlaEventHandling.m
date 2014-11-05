3420e2794383e3a30b102d460be4100d8f420fbf
refaelos <refaeldakar@gmail.com>
2014-11-05 17:16:52 +0200

    Added support for custom events

--- SoomlaiOSCore/SoomlaEventHandling.m
+++ SoomlaiOSCore/SoomlaEventHandling.m
@@ -27,4 +27,12 @@
     [[NSNotificationCenter defaultCenter] postNotificationName:EVENT_REWARD_TAKEN object:self userInfo:userInfo];
 }
 
++ (void)postCustomEventWithName:(NSString*)name andExtraInfo:(NSDictionary*)extra {
+    NSDictionary* userInfo = @{
+                               DICT_ELEMENT_NAME: name,
+                               DICT_ELEMENT_EXTRA: extra
+                               };
+    [[NSNotificationCenter defaultCenter] postNotificationName:EVENT_CUSTOM object:self userInfo:userInfo];
+}
+
 @end
