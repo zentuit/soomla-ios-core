cff53e2dc7ebcf2fd6e8c8444de9f4085f4dfd1c
Eugene Butusov <EvgenyButusov@gmail.com>
2015-08-27 12:42:30 +0300

    3719 [FIX] vendor id changing on iOS
    done

--- SoomlaiOSCore/SoomlaUtils.m
+++ SoomlaiOSCore/SoomlaUtils.m
@@ -32,13 +32,77 @@ static NSString* TAG = @"SOOMLA SoomlaUtils";
     NSLog(@"[*** ERROR ***] %@: %@", tag, msg);
 }
 
-+ (NSString*)deviceIdPreferVendor {
-    if ([[UIDevice currentDevice] respondsToSelector:@selector(identifierForVendor)]) {
-        return [[[UIDevice currentDevice] identifierForVendor] UUIDString];
-    } else {
-        return @"SOOMLA_ID_1234567890";
++(NSString *)deviceIdPreferVendor {
+    NSString *appName = [[NSBundle mainBundle] infoDictionary][(NSString *)kCFBundleNameKey];
+
+    NSString *strApplicationUUID = [self keyForApp:appName];
+    if (strApplicationUUID == nil)
+    {
+        if ([[UIDevice currentDevice] respondsToSelector:@selector(identifierForVendor)]) {
+            strApplicationUUID = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+        } else {
+            strApplicationUUID = @"SOOMLA_ID_1234567890";
+        }
+        [self setForApp:appName key:strApplicationUUID];
     }
+
+    return strApplicationUUID;
 }
+
+- (NSMutableDictionary *)keychainQueryWithService:(NSString *)serviceName andAccount:(NSString *)accountName {
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionaryWithCapacity:3];
+    dictionary[(__bridge id) kSecClass] = (__bridge id) kSecClassGenericPassword;
+    if (serviceName) {
+        dictionary[(__bridge id) kSecAttrService] = serviceName;
+    }
+    if (accountName) {
+        dictionary[(__bridge id) kSecAttrAccount] = accountName;
+    }
+    dictionary[(__bridge id) (kSecAttrSynchronizable)] = (__bridge id)(kSecAttrSynchronizableAny);
+
+    return dictionary;
+}
+
++(void)setForApp:(NSString *)appName key:(NSString *)key {
+    NSString *service = appName;
+    NSString *account = @"incoding";
+
+    NSData *passwordData = [key dataUsingEncoding:NSUTF8StringEncoding];
+
+    NSMutableDictionary *query = [[self class] keychainQueryWithService:service andAccount:account];
+
+#if TARGET_OS_IPHONE
+    SecItemDelete((__bridge CFDictionaryRef)query);
+#else
+	CFTypeRef result = NULL;
+	[query setObject:@YES forKey:(__bridge id)kSecReturnRef];
+	status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
+	if (status == errSecSuccess) {
+		status = SecKeychainItemDelete((SecKeychainItemRef)result);
+		CFRelease(result);
+	}
+#endif
+
+    query[(__bridge id) kSecValueData] = passwordData;
+    SecItemAdd((__bridge CFDictionaryRef)query, NULL);
+}
+
++ (NSString *)keyForApp:(NSString *)appName {
+    NSString *service = appName;
+    NSString *account = @"incoding";
+
+    CFTypeRef result = NULL;
+    NSMutableDictionary *query = [[self class] keychainQueryWithService:service andAccount:account];
+    query[(__bridge id) kSecReturnData] = @YES;
+    query[(__bridge id) kSecMatchLimit] = (__bridge id) kSecMatchLimitOne;
+    SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
+
+    if ([(__bridge_transfer NSData *)result length]) {
+        return [[NSString alloc] initWithData:(__bridge_transfer NSData *)result encoding:NSUTF8StringEncoding];
+    }
+    return nil;
+}
+
 
 /* We check for UDID_SOOMLA to support devices with older versions of ios-store */
 + (NSString*)deviceId {
